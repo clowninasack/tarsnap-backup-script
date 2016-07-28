b84e35403eaaa3604253571e82f4bb25807353eb
clowninasack <clowninasack@users.noreply.github.com>
2016-07-28 10:05:28 -0700

    Added commandline arguments and cleaned some things up.

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # tarsnap-backup-script
-Simple tarsnap backup bourne-shell script with purging functionality. Meant to be run via cron or interactively. Tarsnap key requires reading, writing and deleting permissions and must be unencrypted to be run non-interactively.
+Simple tarsnap backup shell script with purging functionality meant to be run via cron or interactively. Tarsnap key requires reading, writing and deleting permissions and must be unencrypted to be run non-interactively. Archives are named by the folder the script is located in and the date and the time the script is run. Files and directories are to be listed one per line in the `include.txt` and `exclude.txt` files respectively.
 
 ```backup.sh [archives]```
 
 *archives* The number of archives you wish to keep. 
-
-Archives are named by the folder the script is located in and the date and the time the script is run. Files and directories are to be listed one per line in the `include.txt` and `exclude.txt` files respectively.
--- backup.sh
+++ backup.sh
@@ -1,40 +1,65 @@
 #!/bin/sh
-folder=`dirname $0`
-type=`echo ${folder} | rev | cut -f 1 -d '/' | rev`
-date=`date "+%Y-%m-%d"`
+prune=0
+keep=1
+date=`date "+%F_%H-%M"`
+help="backup.sh [-n name] [-p] [-c count]\n\n-n\tSpecify the name of the archives\n-p\tEnable pruning\n-c\tNumber of archives to keep (default: 1)\n"
 
-echo "Starting $type tarsnap backup..."
+while getopts ":hn:pc:" opt; do
+	case $opt in
+	  h)
+	    printf "$help"
+	    exit 0;;
+	  n)
+	    name="$OPTARG_$date";;
+	  p)
+	    prune=1;;
+	  c)
+	    keep=$OPTARG;;
+	  \?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    printf "$help"
+	    exit 1;;
+	  :)
+	    echo "Option -$OPTARG requires an argument." >&2
+	    printf "$help"
+	    exit 1;;
+	esac
+done
 
-/usr/local/bin/tarsnap --quiet -T ${folder}/include.txt -X ${folder}/exclude.txt -cf "${type}_${date}"
+echo "Starting $name tarsnap backup..."
 
-if [ $? -eq 0 ]
+if /usr/local/bin/tarsnap --quiet -cf "$name"
 then
-	echo "Completed $type tarsnap backup."
+	echo "Completed $name tarsnap backup."
 else
-	echo "Failed $type tarsnap backup."
+	echo "Failed $name tarsnap backup."
+	exit 1
 fi
 
-if [ -n $1 ]
+if [ $prune ]
 then
-	keep=$1
-	list=`/usr/local/bin/tarsnap --quiet --list-archives | grep $type | sort`
+	list=`/usr/local/bin/tarsnap --quiet --list-archives | grep $name | sort`
 	wc=`echo "$list" | wc -l`
-	if [ $wc -gt $keep ]
+	if [ $wc -gt $count ]
 	then
 		delete=`expr $wc - $keep`
 	 	list=`echo "$list" | head -n $delete | awk '{print "-f " $0}'`
 
-		echo "Beginning prune of $type tarsnap backup..."
+		echo "Beginning prune of $name tarsnap backup..."
 
-		/usr/local/bin/tarsnap --quiet -d $list
-
-		if [ $? -eq 0 ]
+		if /usr/local/bin/tarsnap --quiet -d $list
 		then
-			echo "Completed prune of $type tarsnap backup."
+			echo "Successfully pruned $delete $name tarsnap backup."
 		else
-			echo "Failed prune of $type tarsnap backup."
+			echo "Failed prune of $name tarsnap backup."
+			exit 1
 		fi
 	fi
 fi
 
-/usr/local/bin/tarsnap --quiet --print-stats --humanize-numbers
+if /usr/local/bin/tarsnap --print-stats --humanize-numbers
+then
+	exit 0
+else
+	exit 1
+fi
